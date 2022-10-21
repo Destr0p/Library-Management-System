6f68fcd3633972196b3bd8a6e732091eb3543c2f
Vishal Gala <109034411+Destr0p@users.noreply.github.com>
2022-10-21 13:37:34 +0530

    Create InvalidBookException.java

---
+++ InvalidBookException.java
@@ -0,0 +1,7 @@
+ public class InvalidBookException extends Exception{
+     	//FILL THE CODE
+         String s;
+         public InvalidBookException(String s){
+             super(s);
+         }
+     }
